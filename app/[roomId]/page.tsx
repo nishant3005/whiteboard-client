c6421d1a7423fa921ac228c9c7b128b3e5430eaf
Nishant Raj <nishantraj3051@gmail.com>
2025-06-16 15:59:22 +0530

    added types

--- app/[roomId]/page.tsx
+++ app/[roomId]/page.tsx
@@ -2,11 +2,17 @@
 
 import dynamic from 'next/dynamic';
 
+type Props = {
+  params: {
+    roomId: string;
+  };
+};
+
 const Whiteboard = dynamic(() => import('../../components/Whiteboard'), {
   ssr: false,
 });
 
-export default function Room({ params }: { params: { roomId: string } }) {
+export default function Room({ params }: Props) {
   return (
     <div className="flex justify-center items-center h-screen">
       <Whiteboard roomId={params.roomId} />
